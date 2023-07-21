9673729deee47b26f3c33e745d0f74ca97ece0c3
joemccaffrey-dev <joe.mccaffrey@hotmail.co.uk>
2023-07-21 12:20:26 +0100

    cosmetic change to bad path formatting

--- src/components/BadPath.jsx
+++ src/components/BadPath.jsx
@@ -1,3 +1,3 @@
 export const BadPath = () => {
-    return <h2>There is no resource at this path, try using the navigation bar links above</h2>
+    return <p className="warning-text">There is no resource at this path, try using the navigation bar links above</p>
 }
